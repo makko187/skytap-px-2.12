51c0282080f1969b3313726245c291f4b5be6821
makko187 <79137474+makko187@users.noreply.github.com>
2022-11-07 14:34:33 +0800

    Update px-op-deploy-skytap.sh
    grafana update

--- px-op-deploy-skytap.sh
+++ px-op-deploy-skytap.sh
@@ -75,10 +75,10 @@ sleep 5
 echo "Installing GRAFANA"
 
 
-kubectl -n kube-system create configmap grafana-dashboard-config --from-file=grafana-dashboard-config.yaml
+kubectl -n portworx create configmap grafana-dashboard-config --from-file=grafana-dashboard-config.yaml
 sleep 5
 
-kubectl -n kube-system create configmap grafana-source-config --from-file=grafana-datasource.yaml
+kubectl -n portworx create configmap grafana-source-config --from-file=grafana-datasource.yaml
 sleep 5
 
 curl "https://docs.portworx.com/samples/k8s/pxc/portworx-cluster-dashboard.json" -o portworx-cluster-dashboard.json && \
@@ -86,7 +86,7 @@ curl "https://docs.portworx.com/samples/k8s/pxc/portworx-node-dashboard.json" -o
 curl "https://docs.portworx.com/samples/k8s/pxc/portworx-volume-dashboard.json" -o portworx-volume-dashboard.json && \
 curl "https://docs.portworx.com/samples/k8s/pxc/portworx-performance-dashboard.json" -o portworx-performance-dashboard.json && \
 curl "https://docs.portworx.com/samples/k8s/pxc/portworx-etcd-dashboard.json" -o portworx-etcd-dashboard.json && \
-kubectl -n kube-system create configmap grafana-dashboards --from-file=portworx-cluster-dashboard.json --from-file=portworx-performance-dashboard.json --from-file=portworx-node-dashboard.json --from-file=portworx-volume-dashboard.json --from-file=portworx-etcd-dashboard.json
+kubectl -n portworx create configmap grafana-dashboards --from-file=portworx-cluster-dashboard.json --from-file=portworx-performance-dashboard.json --from-file=portworx-node-dashboard.json --from-file=portworx-volume-dashboard.json --from-file=portworx-etcd-dashboard.json
 sleep 5
 
 kubectl apply -f grafana.yaml
